6028ac678811c7bf02c62b9f344db110e1a00c70
papoudakis <papoudak@auth.gr>
2016-01-06 04:31:35 +0200

    vectorized version added

--- task1/changefs.m
+++ task1/changefs.m
@@ -8,7 +8,8 @@ t2 = t2(1 : floor(fs2 / fs1 * (size(x, 2) - 1)));
 %     y(i) = (floor(t2(i)) + 1 - t2(i))*x(floor(t2(i))) + (t2(i)-floor(t2(i)))...
 %         *x(floor(t2(i)) + 1);
 % end
-y = interp1(t1, x, t2);
+y = (floor(t2) + 1 - t2).*x(floor(t2)) + (t2 - floor(t2)).*x(floor(t2) + 1);
+% y = interp1(t1, x, t2);
 
 
 

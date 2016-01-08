3b900109dba4c49bf20071035da2260e52d7a19b
papoudakis <papoudak@auth.gr>
2016-01-08 20:44:52 +0200

    adpcm fixed

--- task4/adpcm.m
+++ task4/adpcm.m
@@ -15,9 +15,12 @@ rq = Quant(r, D);
 for i =2 : length(x)
     if i <= m
         r(i) = x(i) - w(1:i-1)'*x(i-1 :-1 : 1);
+        rq(i) = Quant(r(i), D); 
+        x(i) = iQuant(rq(i), L) + w(1:i-1)'*x(i-1:-1: 1);
+        
     else
         r(i) = x(i) - w'*x(i-1 : -1 :i -m);
-    end
-    rq(i) = Quant(r(i), D);
-end
-        
+        rq(i) = Quant(r(i), D);
+        x(i) = iQuant(rq(i), L) + w' * x(i-1 : -1 :i -m);
+    end    
+end      

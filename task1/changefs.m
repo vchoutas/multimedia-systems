99c6ee994d14d3139296f66de307ead15d804b06
vasilish <vasilis4ch@gmail.com>
2016-02-13 17:07:15 +0200

    Use interp1 for signal resampling

--- task1/changefs.m
+++ task1/changefs.m
@@ -1,15 +1,17 @@
-function y = changefs(x, fs1, fs2)
-t1 = 1 :  size(x,2);
-t2 = 1 : fs1 / fs2 : size(x,2);
-t2 = t2(1 : floor(fs2 / fs1 * (size(x, 2) - 1)));
+function y = changefs(x, fs1, fs2, interpMethod)
 
+if nargin < 4
+    interpMethod = 'linear';
+end
 
-% for i=1 : size(t2,2)
-%     y(i) = (floor(t2(i)) + 1 - t2(i))*x(floor(t2(i))) + (t2(i)-floor(t2(i)))...
-%         *x(floor(t2(i)) + 1);
-% end
-y = (floor(t2) + 1 - t2).*x(floor(t2)) + (t2 - floor(t2)).*x(floor(t2) + 1);
-% y = interp1(t1, x, t2);
+t1 = 1:length(x);
+tq = 1 : fs1 / fs2 : size(x,2);
+
+y = interp1(t1, x, tq, interpMethod);
+
+y = y(1:floor(fs2 / fs1 * (length(x) - 1)));
+
+end
 
 
 
